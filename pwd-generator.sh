0909b9bdfa9d4d2c1f97da3af8514ebdf2caddfa
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2024-06-28 08:12:51 +0200

    Update pwd-generator.sh

--- pwd-generator.sh
+++ pwd-generator.sh
@@ -2,17 +2,25 @@
 
 genType=$1
 
-case $genType in
-    "complex")
-        echo "complex"
-        ;;
-    "word-based")
-        echo "word-based"
-        ;;
-    "simple")
-        echo "simple"
-        ;;
-    * )
-        echo "help"
-        ;;
-esac
+main() {
+    case $genType in
+        "complex")
+            echo "complex"
+            ;;
+        "word-based")
+            echo "word-based"
+            ;;
+        "simple")
+            echo "simple"
+            ;;
+        * )
+            echo "help"
+            ;;
+    esac
+}
+
+src=$0
+
+if [ "$0" = "$BASH_SOURCE" ]; then
+    main
+fi
